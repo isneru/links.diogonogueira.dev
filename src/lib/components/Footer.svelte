79e444b44e7c8b39e042475504d15c26976a7bb9
isneru <diogoclanogueira@gmail.com>
2023-04-12 02:24:05 +0100

    Footer link href change to this repo

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,7 @@ import Github from "./icons/Github.svelte"
 <footer class="absolute bottom-0 left-0 right-0 flex h-16 items-center justify-center">
 	<a
 		class="transition-colors hover:text-primary-500"
-		href="https://github.com/isneru"
+		href="https://github.com/isneru/links.diogonogueira.dev"
 		target="_blank"
 		rel="noopener noreferrer"
 		><Github size={24} />
